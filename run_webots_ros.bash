45e857778a6eab1805e06684cde45dc41b30aa1b
DoranLyong <cheeryun@gmail.com>
2021-06-25 11:48:02 +0900

    Update run_webots_ros.bash

--- run_webots_ros.bash
+++ run_webots_ros.bash
@@ -13,6 +13,7 @@ fi
 
 docker run -it \
     --name webots_melodic \
+    --net=host \
     --env="DISPLAY=$DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \

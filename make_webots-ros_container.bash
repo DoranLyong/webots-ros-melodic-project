dd56d36a13af321af702e2cc6dcd6d5001843148
DoranLyong <cheeryun@gmail.com>
2021-10-19 17:43:41 +0900

    Update make_webots-ros_container.bash

--- make_webots-ros_container.bash
+++ make_webots-ros_container.bash
@@ -24,5 +24,5 @@ sudo docker run -t -d \
     --volume="$XAUTH:$XAUTH" \
     --runtime=nvidia \
     -v $HOME:/workspace \
-    doranlyong/webots-ros-melodic:rviz-moveit-v1.0.4 \
+    doranlyong/webots-ros-melodic:rviz-moveit-v1.0.5 \
     bash
